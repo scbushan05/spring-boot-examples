603fda892fea6d269cb082e15e204bae229594c6
scbushan05 <sc.bushan.05@gmail.com>
2021-02-08 23:39:04 +0530

    request param is completed

---
+++ requestparam/src/main/java/in/bushansirgur/requestparam/controller/UserController.java
@@ -0,0 +1,24 @@
+package in.bushansirgur.requestparam.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@GetMapping("/users")
+	public String getName(@RequestParam(name = "userName") String name) {
+		return "My name is "+name;
+	}
+	
+	@GetMapping("/customers")
+	public String getCustomerName(@RequestParam(defaultValue = "anonymous") String name) {
+		return "My name is "+name;
+	}
+	
+	@GetMapping("/employees")
+	public String getEmployeeName(@RequestParam(required = false) String name) {
+		return "My name is "+name;
+	}
+}
---
+++ requestparam/src/test/java/in/bushansirgur/requestparam/RequestparamApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bushansirgur.requestparam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestparamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
